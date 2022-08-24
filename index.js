0af223857e26f172c6d2737dd69e6269f845c168
fliblip <wickynjy@gmail.com>
2022-08-24 09:49:03 +0300

    Done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// code your solution here
+function saturdayFun(hobby = 'roller-skate'){
+    return `This Saturday, I want to ${hobby}!`;
+}
+
+saturdayFun('bathe my dog');
+
+function mondayWork(activity = 'go to the office'){
+    return `This Monday, I will ${activity}.`;
+}
+
+mondayWork('work from home');
+
+function wrapAdjective(wrapper){
+    return function(rank){
+        return (`You are ${wrapper}${rank}${wrapper}!`);
+    };
+}
+
+wrapAdjective("a hard worker")("a dedicated programmer")
+
+// const wrapAdjective = (function (wrapper = "*"){
+
+// })
+
+
